920ac67785c7df2447588a25e43d691dbed04c9f
ZGabc <zofiaostrowska5@gmail.com>
2023-04-12 16:30:55 +0200

    2023-04-12 ZO

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CeneoScraper
 pp2
+
+
+# #Selektory ceneo.pl
+
+
+#tu będzie tabelka z https://word2md.com/
+
+
+# #wykorzystane biblioteki
+-BeautifulSoup4
+-Requests
--- scraper.py
+++ scraper.py
@@ -1,29 +1,41 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+
+def extract_tag(ancestor, selector, attribute=None, return_list=False):
+    try:
+        if return_list:
+            return [tag.text.strip() for tag in ancestor.select(selector)]
+        if not selector and attribute:
+            return ancestor[attribute]
+        if attribute :
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return None 
 
 #product_code =input("podaj kod produktu: ")
 product_code=96693065
-url=f"https://www.ceneo.pl/{product_code}#tab=reviews_scroll"
+url=f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
-opinions = page_dom.select("div.js_product.review")
-all__opinions = []
+opinions = page_dom.select("div.js_product-review")
+all_opinions = []
 for opionion in opinions:
     single_opinion = {
-        "opinion_id": opionion["data-entry-id"],
-        "author":opionion.select_one("span.user__post-name").text.strip(),
-        "recomendation":opionion.select_one("span.user-post_author.recomendation>em").text.strip(),
-        "rating":opionion.select_one("span.user-post__score-count").text.strip(),
-        "verified":opionion.select_one("div.review-pz").text.strip(),
-        "post_date":opionion.select_one("span.user-post__published>time:nth-child(1)")["datetime"].text.strip(),
-        "purchase_date":opionion.select_one("span.user-post__published>time:nth-child(2)")["datetime"].text.strip(),
-        "vote_up":opionion.select_one("button.vote-yes")["data-total-vote"].strip(),
-        "vote_down":opionion.select_one("button.vote-no")["data-total-vote"].strip(),
-        "content":opionion.select_one("div.user-post__text").text.strip(),
-        "pros":[pros.text.strip() for pros in opionion.select_one("div.review-feature__titles—positives~div.review-feature-item")],
-        "cons":[cons.text.strip() for cons in opionion.select_one("div.review-feature__titles—negatives~div.review-feature-item")]
+        "opinion_id": extract_tag(opionion, None, "data-entry-id"),
+        "author": extract_tag(opionion, "span.user__post-name"),
+        "recomendation":extract_tag(opionion,"span.user-post_author.recomendation>em"),
+        "rating":extract_tag(opionion,"span.user-post__score-count"),
+        "verified":extract_tag(opionion,"div.review-pz"),
+        "post_date":extract_tag(opionion,"span.user-post__published>time:nth-child(1)","datetime"),
+        "purchase_date":extract_tag(opionion,"span.user-post__published>time:nth-child(2)","datetime"),
+        "vote_up":extract_tag(opionion,"button.vote-yes","data-total-vote"),
+        "vote_down":extract_tag(opionion,"button.vote-no","data-total-vote"),
+        "content":extract_tag(opionion,"div.user-post__text"),
+        "pros":extract_tag(opionion,"div.review-feature__titles—positives~div.review-feature-item",None, True),
+        "cons":extract_tag(opionion,"div.review-feature__titles—negatives~div.review-feature-item",None, True)
     }
-all__opinions.append(single_opinion)
-
-print(all__opinions)
-
+    all_opinions.append(single_opinion)
+with open("./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
