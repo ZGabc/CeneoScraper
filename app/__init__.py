e611a21e99b997e1ffa241516da2594f926e2644
ZGabc <zofiaostrowska5@gmail.com>
2023-06-09 10:57:32 +0200

    2023-06-07 ZO poprawki

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
-#from flask import Flask
-#app = Flask(__name__)
-#from app import routes
-#app.run(debug=True)
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
