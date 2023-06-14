69c5c96c78cdd922479a4c1f433edb5ffe55531c
ZGabc <zofiaostrowska5@gmail.com>
2023-06-14 16:29:50 +0200

    2023-06-14 ZO

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import os
+import requests
+import json
+from bs4 import BeutifulSuop
+from app.utils import extract_tag, selectors 
 
 @app.route('/')
 @app.route('/index')
@@ -10,17 +15,23 @@ def index():
 def autor():
     return render_template('autor.html')
 
-@app.route('/ekstrakcja')
-def ekstrakcja():
-    return render_template('ekstrakcja.html')
+@app.route('/extract', methods=['POST','GET'])
+def extract():
+    if request.method == "POST":
+        product_code = request.form.get('product_code')
+        return redirect(url_for('product', product_code))
 
-@app.route('/lista_produkt')
-def lista_produkt():
-    return render_template('lista_produkt.html')
+    return render_template('extract.html')
 
-@app.route('/strona_produktu')
-def strona_produktu():
-    return render_template('strona_produktu.html')
+@app.route('/productsTable')
+def productTable():
+    return render_template('productsTable.html')
+
+@app.route('/product/<product_code>')
+def product(product_code):
+    return render_template('product.html', product_code=product_code.
+    to_html(headers=True, classes='table table-striped table-success',
+            table_id='opinions'))
 
 @app.route('/wykresy')
 def wykresy():
